836f9c406a38ce18fbdfcdd6f2b1b26433f41fa1
Shahriar-0 <attarshahriar@gmail.com>
2024-11-15 16:56:59 +0330

    add transaction tests
    heaven was blind to my grief
    
    Co-Authored-By: Mohammad Mahdi Ebrahim Soltani <102657542+MahdiES0@users.noreply.github.com>

--- Fasedyab/src/test/java/domain/TransactionTest.java
+++ Fasedyab/src/test/java/domain/TransactionTest.java
@@ -1,10 +1,42 @@
 package domain;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TransactionTest {
 
+    Transaction transaction;
 
+    @BeforeEach
+    void setup() {
+        transaction = new Transaction();
+        transaction.setTransactionId(1);
+    }
+
+    @Test
+    @DisplayName("Test equals same id")
+    void testEqualsSameId() {
+        Transaction transaction2 = new Transaction();
+        transaction2.setTransactionId(1);
+
+        assertTrue(transaction.equals(transaction2));
+    }
+
+    @Test
+    @DisplayName("Test equals different id")
+    void testEqualsDifferentId() {
+        Transaction transaction2 = new Transaction();
+        transaction2.setTransactionId(2);
+
+        assertFalse(transaction.equals(transaction2));
+    }
+
+    @Test
+    @DisplayName("Test different class")
+    void testEqualsDifferentClass() {
+        assertFalse(transaction.equals(new Object()));
+    }
 }
